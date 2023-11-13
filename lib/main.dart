a890092b1916720cdbdf45d925b263d4c6939060
user <sofiia.bykovska@chisw.com>
2023-11-13 18:18:56 +0200

    basic usage of BehaviorSubject

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  runApp(const App(),);
+  runApp(
+    const App(),
+  );
 }
 
 class App extends StatelessWidget {
@@ -15,29 +17,47 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: false ,
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
 }
 
 class HomePage extends HookWidget {
-  const HomePage({super.key,});
+  const HomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final subject = useMemoized(() => BehaviorSubject<String>(), [key],);
-    
-    useEffect(() => subject.close, [subject],);
+    final subject = useMemoized(
+      () => BehaviorSubject<String>(),
+      [key],
+    );
+
+    useEffect(
+      () => subject.close,
+      [subject],
+    );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Home Page'),
+        title: StreamBuilder<String>(
+            stream: subject.stream.distinct().debounceTime(
+                  const Duration(milliseconds: 500),
+                ),
+            initialData: 'Please start typing...',
+            builder: (context, snapshot) {
+              return Text(
+                snapshot.requireData,
+              );
+            }),
       ),
-      body: TextField(
-        onChanged: (string) {
-          subject.sink.add(string);
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          onChanged: subject.sink.add,
+        ),
       ),
     );
   }
